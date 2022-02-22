b49716ba2122f6574ae10725c13706437fdccae5
Felicia Gillström <fegi2000@student.miun.se>
2022-02-22 19:16:20 +0100

    Add project files.

---
+++ moment3_3/Data/SongContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using moment3_3.Models;
+
+namespace moment3_3.Data;
+    public class SongContext : DbContext
+    {
+        public SongContext(DbContextOptions<SongContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Song> Songs { get; set; }
+
+    }
---
+++ moment3_3/Models/Song.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace moment3_3.Models
+{
+
+    public class Song
+    {
+        //Properties
+        public int Id { get; set; }
+
+        [Required]
+        public string? Artist { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        public int Length { get; set; }
+
+        [Required]
+        public string? Category { get; set; }
+    }
+}
